a55f474687ac27f8af3983b16a7f102fea1307c8
ydaikai <ydaikai@protonmail.com>
2023-11-02 00:33:02 +0900

    modify ydaikai.com

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Humagnett.com';
+export const SITE_TITLE = 'ydaikai.com';
 export const SITE_DESCRIPTION = 'Welcome to my site!';
