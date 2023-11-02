053b694cabc21863d39b9865bb65acfde0ef94ca
ydaikai <ydaikai@protonmail.com>
2023-11-02 12:20:51 +0900

    add favicon

---
+++ tailwind.config.mjs
@@ -0,0 +1,20 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		extend: {},
+	},
+	plugins: [],
+}
+
+module.exports = {
+	theme: {
+	  extend: {
+		fontFamily: {
+		  'noto-sans-jp': ['Noto Sans JP', 'sans-serif'],
+		}
+	  }
+	},
+	variants: {},
+	plugins: [],
+  }
